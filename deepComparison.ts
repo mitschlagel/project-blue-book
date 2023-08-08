3061e5f2e093b524971e19748f60c1004edf703d
Spencer Jones <sjonesdrums@gmail.com>
2023-08-07 20:53:56 -0500

    deep comparison

---
+++ deepComparison.ts
@@ -0,0 +1,14 @@
+function deepEqual(x: any, y: any) {
+    if (typeof x == "object" && typeof y == "object") {
+        if (x == null || y == null) {
+            return false
+        }
+        let xProps = Object.keys(x)
+        let yProps = Object.keys(y)
+        console.log(xProps)
+        console.log(yProps)
+        return xProps.every((current, index) => current === yProps[index])
+    }
+}
+
+console.log(deepEqual({thrr: 1, two: 2},{one: 1, two: 2}))
