d1f50904753dbb3b04978274280acd8d2aef918f
Spencer Jones <sjonesdrums@gmail.com>
2023-08-21 23:42:40 -0500

    promises

---
+++ exercises/promises.js
@@ -0,0 +1,13 @@
+"use strict";
+// promise practice
+let p = Promise.resolve("p");
+console.log(p);
+let w = new Promise((resolve) => {
+    if (typeof resolve === "number") {
+        return resolve + 1;
+    }
+    else {
+        return "oops";
+    }
+});
+console.log(w.then(x => { console.log(x); }));
